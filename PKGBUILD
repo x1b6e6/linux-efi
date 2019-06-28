4d8ec9fb16ec4b4db6e78ca5a854e00c629f630f
DOGINFOG <ftdabcde@gmail.com>
2019-06-28 15:19:53 +0700

    rename to linux-efi
    Signed-off-by: DOGINFOG <ftdabcde@gmail.com>

--- PKGBUILD
+++ PKGBUILD
@@ -1,10 +1,10 @@
 # Maintainer: Jguer <joaogg3@gmail.com>
-pkgname=linux-old
+pkgname=linux-efi
 pkgver=0.1.1
 pkgrel=1
-pkgdesc="old files"
-arch=('i686' 'x86_64' 'armv7h' 'armv6h' 'aarch64')
-depends=('binutils' 'sbsigntools' 'efibootmgr')
+pkgdesc="sign efi hook"
+arch=('any')
+depends=('sbsigntools' 'efibootmgr' 'expect')
 
 source=(
 	"efi_update.sh"
@@ -12,15 +12,13 @@ source=(
 	"reboot-win10"
 	"reboot-grub"
 	"efikeys.tar.xz"
-	"ldlocal.conf"
 )
 sha1sums=(
-	'9a8845398be3b8bd5b3eea963c54981fde74241c'
+	'ac20314fd53ab52ab93e1a7431c29c4baedd088e'
 	'00bba33f5cb898708791cf00b0bfd461d32700d4'
 	'44a1bfb86c0d2605f0fc3ba8fe345c1227dd27ee'
 	'e64a5d44dd4f4b346502a61fd7ad955191c80397'
 	'984f298fb1cd268ddc83bcaac0b868e4d2e8c92e'
-	'ffa441f6aecaff47beedc200626292f0c71fa607'
 )
 
 package() {
@@ -32,6 +30,5 @@ package() {
 	install -Dm600 "key.key" "$pkgdir/etc/efikeys/key.key"
 	install -Dm600 "key.crt" "$pkgdir/etc/efikeys/key.crt"
 	install -Dm600 "efikeys.tar.xz" "$pkgdir/etc/efikeys/efikeys.tar.xz"
-	install -Dm644 "ldlocal.conf" "$pkgdir/etc/ld.so.conf.d/local.conf"
 	chmod -R 600 $pkgdir/etc/efikeys
 }
--- efi_update.sh
+++ efi_update.sh
@@ -4,14 +4,23 @@ LINUX=$1
 BOOTDIR=/boot
 CERTDIR=/etc/efikeys
 KERNEL=$BOOTDIR/vmlinuz-$LINUX
-INITRAMFS=/boot/initramfs-$LINUX.img
+INITRAMFS=/boot/initramfs-${LINUX}.img
 EFISTUB=/usr/lib/systemd/boot/efi/linuxx64.efi.stub
-BUILDDIR=/tmp/_build_efi_update
+TMP=/tmp/$RANDOM
+while $(ls $TMP > /dev/null 2>&1); do
+	TMP=/tmp/$RANDOM
+done
+TMP_INITRD=$TMP/initrd.img
+TMP_BOOT=$TMP/boot.efi
+TMP_BOOT_SIGNED=$TMP/boot-signed.efi
+TMP_EMERGENCY=$TMP/emergency.efi
+TMP_EMERGENCY_SIGNED=$TMP/emergency-signed.efi
+TMP_CMDLINE_BOOT=$TMP/cmdline-boot
+TMP_CMDLINE_EMERGENCY=$TMP/cmdline-emergency
 OUTIMG_DIR=/boot/efi/EFI/$LINUX
-OUTIMG=$OUTIMG_DIR/Bootx64.efi
+OUTIMG_BOOT=$OUTIMG_DIR/Bootx64.efi
+OUTIMG_EMERGENCY=$OUTIMG_DIR/${LINUX}-emergency.efi
 CMDLINED=/etc/cmdline.d
-
-MTMP=false
 
 if [[ $LINUX == "" ]]; then
 	echo "Usage: efi_update [-mk] LINUX"
@@ -40,29 +49,65 @@ fi
 
 [[ $1 == "-mk" ]] && mkinitcpio -p $2
 
-[[ -f /boot/amd-ucode.img ]] && INITRAMFS="$INITRAMFS /boot/amd-ucode.img"
-[[ -f /boot/intel-ucode.img ]] && INITRAMFS="$INITRAMFS /boot/intel-ucode.img"
+[[ -f /boot/amd-ucode.img ]] && INITRAMFS="/boot/amd-ucode.img $INITRAMFS"
+[[ -f /boot/intel-ucode.img ]] && INITRAMFS="/boot/intel-ucode.img $INITRAMFS"
 
-mkdir -p $BUILDDIR
-[[ $MTMP ]] && mount -t tmpfs none $BUILDDIR
-chmod 600 $BUILDDIR
+mkdir -p $TMP
+#mount -t tmpfs none $BUILDDIR
+chmod 600 $TMP
 
-awk -F\# '{print $1}' <<< "`cat $CMDLINED/*`" | sed -e ':a;N;$!ba;s/\n/ /g' -e 's/  */ /g' | tee $BUILDDIR/cmdline
+echo -n "kernel param: "
+CMDLINE=$(grep -vh "#" $CMDLINED/* | sed ':a;N;$!ba;s/\n/ /g')
 
-cat ${INITRAMFS} > ${BUILDDIR}/initramfs.img
+cat ${INITRAMFS} > $TMP_INITRD
+
+echo $CMDLINE "quiet" > ${TMP_CMDLINE_BOOT}
+echo $CMDLINE "emergency" > $TMP_CMDLINE_EMERGENCY
 
 /usr/bin/objcopy \
     --add-section .osrel=/etc/os-release --change-section-vma .osrel=0x20000 \
-    --add-section .cmdline=${BUILDDIR}/cmdline --change-section-vma .cmdline=0x30000 \
+    --add-section .cmdline=$TMP_CMDLINE_BOOT --change-section-vma .cmdline=0x30000 \
     --add-section .linux=${KERNEL} --change-section-vma .linux=0x40000 \
-    --add-section .initrd=${BUILDDIR}/initramfs.img --change-section-vma .initrd=0x3000000 \
-    ${EFISTUB} ${BUILDDIR}/combined-boot.efi
+    --add-section .initrd=$TMP_INITRD --change-section-vma .initrd=0x3000000 \
+    ${EFISTUB} ${TMP_BOOT}
 
-/usr/bin/sbsign --key ${CERTDIR}/key.key --cert ${CERTDIR}/key.crt --output ${BUILDDIR}/combined-boot-signed.efi ${BUILDDIR}/combined-boot.efi
+/usr/bin/objcopy \
+    --add-section .osrel=/etc/os-release --change-section-vma .osrel=0x20000 \
+    --add-section .cmdline=$TMP_CMDLINE_EMERGENCY --change-section-vma .cmdline=0x30000 \
+    --add-section .linux=${KERNEL} --change-section-vma .linux=0x40000 \
+    --add-section .initrd=$TMP_INITRD --change-section-vma .initrd=0x3000000 \
+    ${EFISTUB} ${TMP_EMERGENCY}
+
+while :; do 
+(expect << EOF
+set timeout 120
+spawn /usr/bin/sbsign --key ${CERTDIR}/key.key --cert ${CERTDIR}/key.crt --output ${TMP_BOOT_SIGNED} ${TMP_BOOT}
+expect "*?phrase:*"
+stty -echo
+expect_tty -re "(.*)\\n"
+set pass "\$expect_out(1,string)\n"
+stty echo
+send -- "\$pass"
+expect { 
+	"Signing Unsigned original image" {wait}
+	"*error*" {exit 1}
+}
+	
+
+spawn /usr/bin/sbsign --key ${CERTDIR}/key.key --cert ${CERTDIR}/key.crt --output ${TMP_EMERGENCY_SIGNED} ${TMP_EMERGENCY}
+expect "*?phrase:*"
+send -- "\$pass"
+expect {
+	"Signing Unsigned original image" {wait}
+	"*error*" {exit 1}
+}
+EOF
+) && break;done
 
 mount -o rw,remount /boot/efi
-cp ${BUILDDIR}/combined-boot-signed.efi ${OUTIMG}
+cp ${TMP_BOOT_SIGNED} ${OUTIMG}
+cp ${TMP_EMERGENCY_SIGNED} ${OUTIMG_DIR}/$LINUX-emergency.efi
 mount -o ro,remount /boot/efi
 
-[[ $MTMP ]] && umount $BUILDDIR
+#umount $BUILDDIR
 rm -rf $BUILDDIR
