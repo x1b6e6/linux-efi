bb78eea66dc02274de9def4ea3e9d247940f797d
x1b6e6 <ftdabcde@gmail.com>
2019-12-16 11:01:00 +0700

    remove mod modificator ( work for all kernels )
    Signed-off-by: x1b6e6 <ftdabcde@gmail.com>

--- PKGBUILD
+++ PKGBUILD
@@ -1,8 +1,6 @@
 # Maintainer: x1b6e6 <ftdabcde@gmail.com>
 
-mod=
-
-pkgname=linux${mod}_efi
+pkgname=linux_efi
 pkgver=0.2.0
 pkgrel=1
 pkgdesc="sign efi hook"
@@ -10,34 +8,29 @@ arch=('any')
 depends=('sbsigntools' 'efibootmgr' 'expect' 'openssl' 'efivar')
 
 CMDLINED=/etc/cmdline.d
-TMP=/tmp/linux${mod}_efi
+TMP=/tmp/linux_efi
 TMP_CMDLINED=$TMP/cmdline.di
 EFIKEYS=/etc/efikeys
 
 source=(
-	"linux_efi_upgrade.sh.in"
-	"linux_efi.hook.in"
+	"linux_efi_upgrade.sh"
+	"linux_efi.hook"
 	"linux_efi.install"
 	"ms.esl"
 )
 sha1sums=(
-	'e95bb0017d211b340239901242e557dbd8209900'
-	'9d0e53f52729f1330085e7642ef87d1430bd2c55'
+	'c824c2e06ab8d90e524a35645a33eb24a6146af3'
+	'c9a8e9fdd431b22837e32af123dc91c9eb5f1b8d'
 	'26e4f3bc20677acc2d59691cded9469b72ca014b'
 	'52a296ac12ed09058d227a056cf6d4b3d6ac2760'
 )
 
 install=linux_efi.install
 
-prepare() {
-	sed -e "s/%mod%/$mod/g" $srcdir/linux_efi.hook.in > $srcdir/linux${mod}_efi.hook
-	sed -e "s/%mod%/$mod/g" $srcdir/linux_efi_upgrade.sh.in > $srcdir/linux${mod}_efi_upgrade.sh
-}
-
 package() {
 	cd "$srcdir"
-	install -Dm700 "$srcdir/linux${mod}_efi_upgrade.sh" "$pkgdir/usr/bin/linux${mod}_efi_upgrade"
-	install -Dm644 "$srcdir/linux${mod}_efi.hook" "$pkgdir/usr/share/libalpm/hooks/99-linux${mod}_efi.hook"
+	install -Dm700 "$srcdir/linux_efi_upgrade.sh" "$pkgdir/usr/share/libalpm/scripts/linux_efi_upgrade"
+	install -Dm644 "$srcdir/linux_efi.hook" "$pkgdir/usr/share/libalpm/hooks/99-linux_efi.hook"
 	install -Dm444 "$srcdir/ms.esl" "$pkgdir$EFIKEYS/ms.esl"
 }
 # vim: set ts=4 sw=0 noexpandtab autoindent :
--- linux_efi_upgrade.sh
+++ linux_efi_upgrade.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-LINUX=$1
-LINUX_DEFAULT="%mod%"
-[[ $1 == "-mk" ]] && LINUX=$2
 
-if [[ $LINUX == "" && $LINUX_DEFAULT == "\%mod\%" ]]; then
-	echo "Usage: efi_update [-mk] LINUX"
-	echo
-	echo "    -mk    make initramfs"
-	echo 
-	exit 1
-elif [[ $LINUX == "" && $LINUX_DEFAULT != "\%mod\%" ]]; then
-    LINUX="linux$LINUX_DEFAULT"
-fi
+while read -r pkgbase; do
 
+grep -e "\busr/lib/modules/[[:alnum:][:punct:]]*/vmlinuz\b" <<< "$pkgbase" || continue;
+
+
+KERNEL=/$pkgbase
+pkgbase=$(awk -F/ '{print $4}' <<< "$pkgbase")
+pkgbase=$(awk -F- '{print $4}' <<< "$pkgbase")
+[[ $pkgbase != "" ]] && pkgbase="-$pkgbase"
+pkgbase="linux$pkgbase"
+
+# set 1, if you want generate boot file with emergency cmdline
+GENERATE_EMERGENCY=0
+
+LINUX=$pkgbase
 BOOTDIR=/boot
 CERTDIR=/etc/efikeys
-KERNEL=$BOOTDIR/vmlinuz-$LINUX
+# KERNEL=$BOOTDIR/vmlinuz-$LINUX
 INITRAMFS=/boot/initramfs-${LINUX}.img
 EFISTUB=/usr/lib/systemd/boot/efi/linuxx64.efi.stub
 TMP=/tmp/$RANDOM
@@ -36,6 +38,31 @@ CMDLINED=/etc/cmdline.d
 EFI_KEY=/etc/efi.key.pem
 EFI_CRT=/etc/efi.pub.pem
 
+EXPECT_COMMANDS="
+set timeout 120
+spawn /usr/bin/sbsign --key $EFI_KEY --cert $EFI_CRT --output ${TMP_BOOT_SIGNED} ${TMP_BOOT}
+expect \"*?phrase:*\"
+stty -echo
+expect_tty -re \"(.*)\\n\"
+set pass \"\$expect_out(1,string)\n\"
+stty echo
+send -- \"\$pass\"
+expect {
+        \"Signing Unsigned original image\" {wait}
+        \"bad decrypt\" {exit 1}
+        \"bad password\" {exit 2}
+}"
+
+EXPECT_COMMANDS_EMERGENCY="
+spawn /usr/bin/sbsign --key $EFI_KEY --cert $EFI_CRT --output ${TMP_EMERGENCY_SIGNED} ${TMP_EMERGENCY}
+expect \"*?phrase:*\"
+send -- \"\$pass\"
+expect {
+        \"Signing Unsigned original image\" {wait}
+        \"bad decrypt\" {exit 1}
+        \"bad password\" {exit 2}
+}"
+
 if [[ ! -x $EFISTUB ]]; then
 	echo "efi stub not found!"
 	exit 1
@@ -52,8 +79,6 @@ if [[ -d $BUILDDIR ]]; then
 	exit 1
 fi
 
-[[ $1 == "-mk" ]] && mkinitcpio -p $LINUX
-
 [[ -f /boot/amd-ucode.img ]] && INITRAMFS="/boot/amd-ucode.img $INITRAMFS"
 [[ -f /boot/intel-ucode.img ]] && INITRAMFS="/boot/intel-ucode.img $INITRAMFS"
 
@@ -61,16 +86,19 @@ mkdir -p $TMP
 #mount -t tmpfs none $BUILDDIR
 chmod 600 $TMP
 
-echo -n "kernel param: "
 
-#CMDLINE=$(grep -vh "#" $CMDLINED/* | sed ':a;N;$!ba;s/\n/ /g')
+# generating cmdline from files /etc/cmdline.d/*
+echo -n "kernel param: "
 CMDLINE=$(find $CMDLINED -type f -exec awk -F '#' '{printf $1 " "}' {} \;)
 echo $CMDLINE
 
+
+# create full initramfs
 cat ${INITRAMFS} > $TMP_INITRD
 
 echo $CMDLINE "quiet" > ${TMP_CMDLINE_BOOT}
-echo $CMDLINE "emergency" > $TMP_CMDLINE_EMERGENCY
+(( GENERATE_EMERGENCY )) && 
+echo $CMDLINE "emergency" > ${TMP_CMDLINE_EMERGENCY}
 
 /usr/bin/objcopy \
     --add-section .osrel=/etc/os-release --change-section-vma .osrel=0x20000 \
@@ -79,6 +107,7 @@ echo $CMDLINE "emergency" > $TMP_CMDLINE_EMERGENCY
     --add-section .initrd=$TMP_INITRD --change-section-vma .initrd=0x3000000 \
     ${EFISTUB} ${TMP_BOOT}
 
+(( GENERATE_EMERGENCY )) &&
 /usr/bin/objcopy \
     --add-section .osrel=/etc/os-release --change-section-vma .osrel=0x20000 \
     --add-section .cmdline=$TMP_CMDLINE_EMERGENCY --change-section-vma .cmdline=0x30000 \
@@ -89,31 +118,9 @@ echo $CMDLINE "emergency" > $TMP_CMDLINE_EMERGENCY
 if grep -i ENCRYPTED $EFI_KEY 2>&1 > /dev/null; then
 
 while :; do 
-	expect << EOF
-set timeout 120
-spawn /usr/bin/sbsign --key $EFI_KEY --cert $EFI_CRT --output ${TMP_BOOT_SIGNED} ${TMP_BOOT}
-expect "*?phrase:*"
-stty -echo
-expect_tty -re "(.*)\\n"
-set pass "\$expect_out(1,string)\n"
-stty echo
-send -- "\$pass"
-expect { 
-	"Signing Unsigned original image" {wait}
-	"bad decrypt" {exit 1}
-	"bad password" {exit 2}
-}
-	
-
-spawn /usr/bin/sbsign --key $EFI_KEY --cert $EFI_CRT --output ${TMP_EMERGENCY_SIGNED} ${TMP_EMERGENCY}
-expect "*?phrase:*"
-send -- "\$pass"
-expect {
-	"Signing Unsigned original image" {wait}
-	"bad decrypt" {exit 1}
-	"bad password {exit 2}
-}
-EOF
+    (( GENERATE_EMERGENCY )) &&
+	(expect <<< "$EXPECT_COMMANDS$EXPECT_COMMANDS_EMERGENCY") || 
+    (expect <<< "$EXPECT_COMMANDS")
 	sig=$?
 	case $sig in
 		0)break;;
@@ -122,12 +129,18 @@ EOF
 done
 else
     /usr/bin/sbsign --key $EFI_KEY --cert $EFI_CRT --output $TMP_BOOT_SIGNED $TMP_BOOT
+    (( GENERATE_EMERGENCY )) &&
     /usr/bin/sbsign --key $EFI_KEY --cert $EFI_CRT --output $TMP_EMERGENCY_SIGNED $TMP_EMERGENCY
 fi
 mount -o rw,remount /boot/efi
 cp ${TMP_BOOT_SIGNED} ${OUTIMG_BOOT}
+(( GENERATE_EMERGENCY )) &&
 cp ${TMP_EMERGENCY_SIGNED} ${OUTIMG_DIR}/$LINUX-emergency.efi
 mount -o ro,remount /boot/efi
 
 #umount $BUILDDIR
 rm -rf $BUILDDIR
+
+
+# TODO: adding to efi boot list
+done
