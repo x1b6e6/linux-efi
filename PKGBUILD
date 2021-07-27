3308c5d66d3cf2b2fe441b883e14d1ba14a4c8b0
x1b6e6 <ftdabcde@gmail.com>
2021-07-27 20:58:09 +0700

    delegate sign function to `linux-sign` package

--- PKGBUILD
+++ PKGBUILD
@@ -1,43 +1,25 @@
 # Maintainer: x1b6e6 <ftdabcde@gmail.com>
 
 pkgname=linux-efi
-pkgver=0.3.1
+pkgver=1.0.0
 pkgrel=1
 pkgdesc="sign efi hook"
 arch=('any')
-depends=('sbsigntools' 'efibootmgr' 'expect' 'openssl' 'efivar' 'llvm' 'efitools')
-
-INSTALL_MS_THIRDPARTY=1 # for thirdparty devices and software signed with Microsoft (such as VeraCrypt)
-INSTALL_MS_ROOT=1       # for booting official Windows
+depends=(
+	'pacman'
+	'linux-sign'
+)
 
 source=(
 	"linux_efi_upgrade.sh"
 	"linux_efi.hook"
-	"linux_efi.install"
-	"msRoot.esl"
-	"msThirdParty.esl"
 )
-sha1sums=('8ff9f72430ba7de66086a8e10b65dbecfacb62ff'
-          '7e280fafba7f5acc96488d30e694dcf45f09a979'
-          '0fb1f04063866191c6a3d511d3e44e719a79a003'
-          'db7ef2c3bcb35979607abad0c6f415546b7da003'
-          '22594e7c709142c790bf56925c203544e433c148')
-
-prepare(){
-	# clean file
-	echo "" > $srcdir/ms.esl
-
-	# install additional certs
-	((INSTALL_MS_ROOT)) && cat $srcdir/msRoot.esl >> ms.esl
-	((INSTALL_MS_THIRDPARTY)) && cat $srcdir/msThirdParty.esl >> ms.esl
-}
-
-install=linux_efi.install
+sha1sums=('1f05c24c60f80b8d1be61dcffbfebc36233b4aaa'
+          '7e280fafba7f5acc96488d30e694dcf45f09a979')
 
 package() {
 	cd "$srcdir"
 	install -Dm700 "$srcdir/linux_efi_upgrade.sh" "$pkgdir/usr/share/libalpm/scripts/linux_efi_upgrade"
 	install -Dm644 "$srcdir/linux_efi.hook" "$pkgdir/usr/share/libalpm/hooks/99-linux_efi.hook"
-	install -Dm444 "$srcdir/ms.esl" "$pkgdir/etc/efikeys/ms.esl"
 }
 # vim: set ts=4 sw=0 noexpandtab autoindent :
--- linux_efi_upgrade.sh
+++ linux_efi_upgrade.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# constants
-CERT_DIR="/etc/efikeys"
-EFI_KEY="/etc/efi.key.pem"
-EFI_CRT="/etc/efi.pub.pem"
-EFISTUB="/usr/lib/systemd/boot/efi/linuxx64.efi.stub"
-ADDITIONAL_INITRAMFS="/boot/amd-ucode.img /boot/intel-ucode.img"
-
 # list of files what need update (generating automaticaly)
 KERN_FILES=""
 
@@ -39,66 +32,8 @@ for KFile in ${KERN_FILES}; do
 
 	echo UPDATE ${PKG} >&2
 
-	INITRAMFS="/boot/initramfs-${PKG}.img"
-	CMDLINE="/etc/cmdline-${PKG}"
-	[[ ! -f $CMDLINE ]] && cp /proc/cmdline $CMDLINE
-	
-	TMP_EFI_APPLICATION="/tmp/linux-efi-${PKG}-application.efi"
-	TMP_INITRAMFS="/tmp/linux-efi-initramfs.img"
+	linux-sign "${PKG}" "/boot/EFI/${PKG}/Bootx64.efi"
 
-	# DON'T TOUCH NEXT LINE
-	TMP_EFI_APPLICATION_SIGNED="/tmp/linux-efi-${PKG}-application-signed.efi"
-
-	cat ${ADDITIONAL_INITRAMFS} ${INITRAMFS} > ${TMP_INITRAMFS} 2>/dev/null
-
-	/usr/bin/llvm-objcopy \
-		-R .osrel \
-		-R .cmdline \
-		-R .linux \
-		-R .initrd \
-		${EFISTUB} ${TMP_EFI_APPLICATION}
-
-	# add sections
-	/usr/bin/llvm-objcopy \
-        --add-section .osrel=/etc/os-release        \
-        --add-section .cmdline=${CMDLINE}           \
-        --add-section .linux=${KERNEL_IMAGE}        \
-        --add-section .initrd=${TMP_INITRAMFS}      \
-        ${TMP_EFI_APPLICATION} ${TMP_EFI_APPLICATION}
-	
-	# change vma of sections
-	/usr/bin/objcopy \
-        --change-section-vma .osrel=0x20000    \
-        --change-section-vma .cmdline=0x30000  \
-        --change-section-vma .linux=0x40000    \
-        --change-section-vma .initrd=0x3000000 \
-        ${TMP_EFI_APPLICATION} ${TMP_EFI_APPLICATION}
-
-	/usr/bin/sbsign                            \
-		--key ${EFI_KEY}                       \
-		--cert ${EFI_CRT}                      \
-		--output ${TMP_EFI_APPLICATION_SIGNED} \
-		${TMP_EFI_APPLICATION}
-
-	PKGS_UPDATE="${PKGS_UPDATE} ${PKG}"
-
-	# clear temporary files
-	rm ${TMP_EFI_APPLICATION} ${TMP_INITRAMFS}
-done
-
-for PKG in $PKGS_UPDATE; do
-	EFI_APPLICATION_DIR=/boot/EFI/${PKG}
-	EFI_APPLICATION=${EFI_APPLICATION_DIR}/Bootx64.efi
-
-	# DON'T TOUCH NEXT LINE
-	TMP_EFI_APPLICATION_SIGNED="/tmp/linux-efi-${PKG}-application-signed.efi"
-
-	# copy result to efi partition
-	mkdir -p ${EFI_APPLICATION_DIR}
-	cp ${TMP_EFI_APPLICATION_SIGNED} ${EFI_APPLICATION}
-
-	# remove temporary signed efi application
-	rm ${TMP_EFI_APPLICATION_SIGNED}
 done
 
 # vim: set ts=4 sw=4 :
